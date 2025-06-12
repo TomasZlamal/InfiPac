95262cb62b50ba70e590243d180fd5bcf12a2c37
Tomáš Zlámal <zlamal@tomaszlamal.com>
2025-06-12 16:47:10 +0200

    Fixed minor bug

--- src/rectanglerenderer.cc
+++ src/rectanglerenderer.cc
@@ -5,7 +5,7 @@
 namespace pac {
 void RectangleRenderer::render() const {
   auto rrb = dynamic_cast<RectangleRigidBody *>(m_rigidbody.get());
-  DrawRectangle(rrb->pos.x, rrb->pos.y, rrb->wh.x, rrb->wh.y,
-                (Color){230, 41, 55, 255});
+  DrawRectangle(rrb->pos.x - rrb->wh.x / 2, rrb->pos.y - rrb->wh.y / 2,
+                rrb->wh.x, rrb->wh.y, (Color){230, 41, 55, 255});
 }
 } // namespace pac
